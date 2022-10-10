68af681883d059baca96b20524ddde357052c830
Mubashir Kabir <97.m.kabir@gmail.com>
2022-10-10 22:13:10 +0600

    home page banner component created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Banner } from "./Components/Banner";
 import { Footer } from "./Components/Footer";
 import { Navbar } from "./Components/Navbar";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Banner></Banner>
       <Footer></Footer>
     </div>
   );
