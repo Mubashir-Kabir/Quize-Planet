141553644a7e91b6c9613adee6d285f8a8aa18c6
Mubashir Kabir <97.m.kabir@gmail.com>
2022-10-11 16:53:56 +0600

    quiz selection ux updated

--- src/Components/QuizCard.jsx
+++ src/Components/QuizCard.jsx
@@ -1,5 +1,4 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
 import { faEye, faXmark } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
@@ -39,13 +38,13 @@ const QuizCard = ({ ques }) => {
   return (
     <div className="relative">
       {toggle && (
-        <div className="absolute rounded-md top-2 bg-amber-400 text-gray-200 px-12 py-6 right-12 z-10">
+        <div className="absolute rounded-md top-2 bg-white text-gray-600 px-12 py-6 right-12 z-10">
           <div className="relative">
             <h1 className="text-2xl font-semibold">The Answer is:</h1>
             <p>{correctAnswer}</p>
             <FontAwesomeIcon
               onClick={() => setToggle(!toggle)}
-              className="absolute -bottom-4 -right-10  text-xl px-3 rounded-full py-2 bg-amber-500 hover:cursor-pointer hover:bg-amber-600"
+              className="absolute -top-4 -right-10  text-xl px-3 rounded-full py-2 bg-amber-500 hover:cursor-pointer hover:bg-amber-600"
               icon={faXmark}
             ></FontAwesomeIcon>
           </div>
@@ -64,20 +63,38 @@ const QuizCard = ({ ques }) => {
           icon={faEye}
         ></FontAwesomeIcon>
 
-        <ul className="grid gap-3 md:grid-cols-2 ">
+        <div className="grid gap-3 md:grid-cols-2 ">
           {options.map((option) => (
-            <li
-              onClick={() => notify(option)}
-              className="flex text-xl items-center space-x-2 border border-amber-400 px-4 py-8 rounded-md hover:bg-amber-400 hover:text-white hover:cursor-pointer"
-            >
-              <FontAwesomeIcon icon={faCircleCheck}></FontAwesomeIcon>
-              <span>{option}</span>
-            </li>
+            <div class="form-check pl-3 flex items-center border rounded-md  border-amber-400 ">
+              <input
+                className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-amber-400 checked:border-amber-400 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                type="radio"
+                name={question}
+                id={option}
+                onClick={() => notify(option)}
+              />
+              <label
+                class="form-check-label w-full h-full inline-block  text-xl  space-x-2  px-4 py-8 bg-amber-400 rounded-md text-white cursor-pointer "
+                htmlFor={option}
+              >
+                {option}
+              </label>
+            </div>
           ))}
-        </ul>
+        </div>
       </div>
     </div>
   );
 };
 
 export default QuizCard;
+
+{
+  /* <li
+  onClick={() => notify(option)}
+  className="flex text-xl items-center space-x-2 border border-amber-400 px-4 py-8 rounded-md hover:bg-amber-400 hover:text-white hover:cursor-pointer"
+>
+  <FontAwesomeIcon icon={faCircleCheck}></FontAwesomeIcon>
+  <span>{option}</span>
+</li>; */
+}
